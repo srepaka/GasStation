8460078edde31ba09342dba4cb430532c3c56943
Sandeep Repaka <rsandeep86@gmail.com>
2015-11-18 23:14:18 -0800

    Add basic validation to payouts model

--- db/migrate/20151115202953_create_payouts.rb
+++ db/migrate/20151115202953_create_payouts.rb
@@ -1,9 +1,9 @@
 class CreatePayouts < ActiveRecord::Migration
   def change
     create_table :payouts do |t|
-      t.text :description
+      t.text :description, length: { minimum: 3 }, null: false
       t.decimal :amount, precision: 5, scale: 2, null: false
-      t.references :daily_report, index: true, foreign_key: true
+      t.references :daily_report, index: true, foreign_key: true, null: false
       t.integer :category, null: false
 
       t.timestamps null: false
